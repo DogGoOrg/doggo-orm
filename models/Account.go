e446102f6f0966405a7711fdad93ef9c414417c1
lesleysin <nikandsin@gmail.com>
2023-06-26 22:34:11 +0200

    fc

---
+++ models/Account.go
@@ -0,0 +1,10 @@
+package models
+
+import "gorm.io/gorm"
+
+type Account struct {
+	gorm.Model
+	Email    string `gorm:"type:varchar(200);UNIQUE"`
+	Password string `gorm:"type:varchar(200);"`
+	FullName string `gorm:"type:varchar(200);"`
+}
---
+++ models/Pet.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type Pet struct {
+	gorm.Model
+	Nickname string  `gorm:"type:varchar(200);"`
+	Species  string  `gorm:"type:varchar(200);"`
+	Breed    string  `gorm:"type:varchar(200);"`
+	Owner    Account `gorm:"foreignKey:AccountID"`
+}
